3a284a1063e4a50c36815d060ab1354dc36556bd
shellbear <shellbear@protonmail.com>
2021-07-03 18:52:17 +0900

    Redesign and improve Experiences pages

--- pages/about/[slug].tsx
+++ pages/about/[slug].tsx
@@ -1,153 +1,71 @@
-import React from 'react';
-import glob from 'fast-glob';
-import { Container, Title, Text, Grid } from '../../components';
-import { join } from 'path';
-import { NextPage, GetStaticPaths, GetStaticPropsResult } from 'next';
-import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
-import { GrayMatterFile } from 'gray-matter';
+import { GetStaticPaths, GetStaticPropsResult, NextPage } from 'next';
+import { NotionRenderer, NotionRendererProps, Code } from 'react-notion-x';
+import { NotionAPI } from 'notion-client';
+import { getPageInfo, Page, EXPERIENCES } from '../../posts/notion';
+import { Container } from '../../components';
 import Head from 'next/head';
-import TransparentLink from '../../components/TransparentLink';
-import { getPosts, Post } from '../../posts';
 
-interface AboutProps {
-  data: GrayMatterFile<string>['data'];
-  source: MDXRemoteSerializeResult;
-  experiences: Post[];
+interface BlogProps {
+  page: Page;
+  recordMap: NotionRendererProps['recordMap'];
 }
 
-const About: NextPage<AboutProps> = ({ experiences, data, source }) => (
-  <Container maxWidth={1200}>
-    <Container
-      alignItems="center"
-      justifyContent="center"
-      alignContent="center"
-    >
-      <Head>
-        <title>{data.title}</title>
-      </Head>
-      <Grid gridTemplateColumns="1fr" justifyItems="center">
-        <img alt="logo" src={data.preview} width="100rem" height="auto" />
-        <Title fontSize="2rem" marginTop="2rem" as="h2">
-          {data.post} @ {data.title}
-        </Title>
-        <Text margin={0}>{data.date}</Text>
-      </Grid>
-    </Container>
-    <MDXRemote {...source} />
-    <Container
-      alignContent="center"
-      textAlign="center"
-      borderBottom="1px solid rgba(0,0,0,0.1)"
-      padding="40px 0"
-    >
-      <Text fontSize="16px" lineHeight="35px">
-        <a href={data.url}>website</a>
-      </Text>
-    </Container>
-    <Container width="100%" alignContent="center">
-      <Container
-        width="100%"
-        marginTop="4rem"
-        gridGap="2rem"
-        maxWidth={['100%', '800px']}
-      >
-        <Title fontSize="2rem" as="h3">
-          Other experiences
-        </Title>
-        <Container width="100%">
-          {experiences.map(({ data }, i) => (
-            <TransparentLink href={`/about/${data.slug}`} key={data.slug}>
-              <Grid
-                key={i}
-                gridTemplateColumns="1fr 4fr"
-                justifyItems="flex-start"
-                gridGap="1rem"
-                paddingY="2rem"
-                borderBottom="1px solid rgba(0,0,0,0.1)"
-              >
-                <Container width="100%">
-                  <Text>0{experiences.length - i}</Text>
-                </Container>
-                <Grid width="100%" gridTemplateColumns="4fr 1fr">
-                  <Container
-                    width="100%"
-                    alignItems="flex-start"
-                    textAlign="start"
-                  >
-                    <Grid
-                      width="100%"
-                      gridTemplateColumns="repeat(2, auto)"
-                      justifyItems="flex-start"
-                      justifyContent="flex-start"
-                      gridGap="1rem"
-                    >
-                      <Title fontSize="1.5rem" margin={0} as="h3">
-                        {data.title}
-                      </Title>
-                      <Text
-                        fontSize="smaller"
-                        margin={0}
-                        color="rgba(0, 0, 0, 0.1)"
-                      >
-                        {data.date}
-                      </Text>
-                    </Grid>
-                    <Text fontSize="1rem">{data.caption}</Text>
-                  </Container>
-                  <Text fontSize="1.5rem">&rarr;</Text>
-                </Grid>
-              </Grid>
-            </TransparentLink>
-          ))}
-        </Container>
-      </Container>
-    </Container>
+const About: NextPage<BlogProps> = ({ page, recordMap }) => (
+  <Container
+    width={['100%', 1200]}
+    maxWidth="100vw"
+    marginBottom={['1rem', '4rem']}
+  >
+    <Head>
+      <title>{page.title}</title>
+      <meta property="og:title" content={page.title} />
+    </Head>
+    <NotionRenderer
+      fullPage
+      className="notion-container"
+      recordMap={recordMap}
+      components={{
+        code: Code,
+      }}
+    />
   </Container>
 );
 
-type Params = {
-  params: {
-    slug: string;
-  };
-};
-
-const baseDir = join(process.cwd(), './posts/experiences/');
-const contentGlob = join(baseDir, '/*.mdx');
-
 export const getStaticPaths: GetStaticPaths = async () => {
-  const files = glob.sync(contentGlob);
-  const paths = files.map((file) => {
-    const split = file.split('/');
-    const filename = split[split.length - 1];
-    const slug = filename.replace('.mdx', '');
-
-    return {
+  return {
+    paths: Object.keys(EXPERIENCES).map((slug) => ({
       params: {
         slug,
       },
-    };
-  });
-
-  return {
-    paths,
+    })),
     fallback: false,
   };
 };
 
+type Params = {
+  params: {
+    slug: keyof typeof EXPERIENCES;
+  };
+};
+
+const notion = new NotionAPI();
+
 export const getStaticProps = async ({
   params: { slug },
-}: Params): Promise<GetStaticPropsResult<AboutProps>> => {
-  const experiences = await getPosts('experiences');
-  const index = experiences.findIndex(({ data }) => data.slug === slug);
-  const { data, source } = experiences[index];
+}: Params): Promise<GetStaticPropsResult<BlogProps>> => {
+  const { uri, date } = EXPERIENCES[slug];
+  const recordMap = await notion.getPage(uri);
+  const pageInfo = getPageInfo(recordMap);
+  const page: Page = {
+    ...pageInfo,
+    uri,
+    date,
+  };
 
   return {
     props: {
-      experiences: experiences.sort((a, b) =>
-        b.data.date.toString().localeCompare(a.data.date.toString()),
-      ),
-      data,
-      source,
+      page,
+      recordMap,
     },
   };
 };
--- posts/notion.ts
+++ posts/notion.ts
@@ -12,6 +12,29 @@ export const POSTS = {
   },
 };
 
+export const EXPERIENCES = {
+  shareview: {
+    date: '2020 - Now',
+    uri: 'shblog/Shareview-CTO-b3f7d62a744d4cb7bd271d25f4790ece',
+  },
+  dispatcher: {
+    date: '2020',
+    uri: 'shblog/Dispatcher-Go-Backend-developer-fa13680ef8324bacb31b773a5f08c3ad',
+  },
+  tresorio: {
+    date: '2019 - 2020',
+    uri: 'shblog/Tresorio-12516126ddb84eb6abc72656731e1f26',
+  },
+  epitech: {
+    date: '2018 - 2019',
+    uri: 'shblog/Epitech-Teaching-assistant-b8ee49774bd5423ba00a6921c026c1c6',
+  },
+  bilberry: {
+    date: '2018 - 2020',
+    uri: 'shblog/Bilberry-28dc7324b3e24fd6a4475a63c2ac0410',
+  },
+};
+
 export interface PageInfo {
   title: string;
   cover?: string;
@@ -29,7 +52,7 @@ export const getPageInfo = (page: ExtendedRecordMap): PageInfo => {
   };
 
   const block = Object.values(page.block)[0].value;
-  if (block.type === 'page' && block.format.page_cover) {
+  if (block.type === 'page' && block.format?.page_cover) {
     info.coverPosition = block.format.page_cover_position;
     info.cover =
       'https://www.notion.so/image/' +
