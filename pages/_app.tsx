b5b486a2c9cb6b6568c1ecbe48272291fc2a2c79
shellbear <shellbear@protonmail.com>
2023-05-04 21:41:30 +0200

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { motion } from 'framer-motion';
+import { Analytics } from '@vercel/analytics/react';
 
 import { Layout } from '@components';
 
@@ -15,15 +16,18 @@ import 'prismjs/components/prism-graphql';
 import 'prismjs/components/prism-bash';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Layout>
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.2 }}
-    >
-      <Component {...pageProps} />
-    </motion.div>
-  </Layout>
+  <>
+    <Analytics />
+    <Layout>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.2 }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </Layout>
+  </>
 );
 
 export default App;
